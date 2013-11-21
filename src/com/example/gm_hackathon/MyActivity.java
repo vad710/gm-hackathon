3f0c98c34738d138d9b209b3491b1f0578c9f8d9
vad710 <vinny.dasilva@gmail.com>
2013-11-20 21:04:54 -0500

    Removed the Calendar stuff and added Gallery

--- src/com/example/gm_hackathon/MyActivity.java
+++ src/com/example/gm_hackathon/MyActivity.java
@@ -3,11 +3,17 @@ package com.example.gm_hackathon;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentUris;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
 
 
 import java.text.DateFormat;
@@ -16,7 +22,20 @@ import java.util.Calendar;
 
 //import java.util.Calendar;
 
+//variable for selection intent
+
+
 public class MyActivity extends Activity {
+
+    //variable for selection intent
+    private final int PICKER = 1;
+    //variable to store the currently selected image
+    private int currentPic = 0;
+    //gallery object
+    private Gallery picGallery;
+    //image view for larger display
+    private ImageView picView;
+
     /**
      * Called when the activity is first created.
      */
@@ -24,68 +43,34 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        this.InitializeControls();
     }
 
-    public static final String[] INSTANCE_PROJECTION = new String[] {
-            CalendarContract.Instances.EVENT_ID,      // 0
-            CalendarContract.Instances.BEGIN,         // 1
-            CalendarContract.Instances.TITLE          // 2
-    };
+    private void InitializeControls()
+    {
+        Button selectPhotoButton= (Button) findViewById(R.id.selectPictureButton);
 
-    private static final int PROJECTION_ID_INDEX = 0;
-    private static final int PROJECTION_BEGIN_INDEX = 1;
-    private static final int PROJECTION_TITLE_INDEX = 2;
+        View.OnClickListener pickPhotoListener = new View.OnClickListener() {
+            public void onClick(View v){
+                PickAPhoto();
+            }
+        };
 
-    private static final String DEBUG_TAG = "MyActivity";
+        selectPhotoButton.setOnClickListener(pickPhotoListener);
 
-    private void LoadCalendarInfo()
+    }
+
+    private void PickAPhoto()
     {
 
-        // Specify the date range you want to search for recurring
-        // event instances
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2011, 9, 23, 8, 0);
-        long startMillis = beginTime.getTimeInMillis();
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2011, 10, 24, 8, 0);
-        long endMillis = endTime.getTimeInMillis();
 
-        Cursor cur = null;
-        ContentResolver cr = getContentResolver();
-
-        // The ID of the recurring event whose instances you are searching
-        // for in the Instances table
-        String selection = CalendarContract.Instances.EVENT_ID + " = ?";
-        String[] selectionArgs = new String[] {"207"};
-
-        // Construct the query with the desired date range.
-        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
-        ContentUris.appendId(builder, startMillis);
-        ContentUris.appendId(builder, endMillis);
-
-        // Submit the query
-        cur =  cr.query(builder.build(),
-                INSTANCE_PROJECTION,
-                selection,
-                selectionArgs,
-                null);
-
-        while (cur.moveToNext()) {
-            String title = null;
-            long eventID = 0;
-            long beginVal = 0;
-
-            // Get the field values
-            eventID = cur.getLong(PROJECTION_ID_INDEX);
-            beginVal = cur.getLong(PROJECTION_BEGIN_INDEX);
-            title = cur.getString(PROJECTION_TITLE_INDEX);
-
-            // Do something with the values.
-            Log.i(DEBUG_TAG, "Event:  " + title);
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(beginVal);
-            DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-            Log.i(DEBUG_TAG, "Date: " + formatter.format(calendar.getTime()));
-        }
+        //take the user to their chosen image selection app (gallery or file manager)
+        Intent pickIntent = new Intent();
+        pickIntent.setType("image/*");
+        pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+        //we will handle the returned data in onActivityResult
+        startActivityForResult(Intent.createChooser(pickIntent, "Select Picture"), PICKER);
     }
 }
+
